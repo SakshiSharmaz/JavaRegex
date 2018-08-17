3018800169d92bd28a23e80ecfedeb778f2dfe42
Sakshi Sharma <sakshiss.7699@gmail.com>
2018-08-17 12:44:44 +0530

    First Commit

---
+++ src/main/java/JavaRegex/App.java
@@ -0,0 +1,18 @@
+package JavaRegex;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException {
+        System.out.println( "Hello World!" );
+        regexPattern rh = new regexPattern();
+        rh.regex("^import.*;");
+        rh.regex("0");
+
+    }
+}
---
+++ src/main/java/JavaRegex/regexPattern.java
@@ -0,0 +1,26 @@
+package JavaRegex;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class regexPattern {
+    public void regex(String s) throws IOException {
+        int count = 0 ;
+
+        BufferedReader br=new BufferedReader(new FileReader("/home/juno/testregex.txt"));
+        String line;
+        while((line=br.readLine())!=null){
+            Pattern p=Pattern.compile(s);
+            Matcher m=p.matcher(line);
+
+            while (m.find()) {
+                System.out.println(line);
+                count++;
+            }
+        }
+
+        System.out.println("Total values found " + count);
+    }
+}
